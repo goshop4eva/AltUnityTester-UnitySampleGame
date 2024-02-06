3bea113609ae23019ca36e4a664dea1c663d254f
Simon <goshop4eva@gmail.com>
2024-02-06 12:10:45 +0000

    Added page objects to Unity Tester Project

---
+++ AltTester/Assets/AltTester/Editor/pages/GetAnotherChancePage.cs
@@ -0,0 +1,30 @@
+using AltTester.AltTesterUnitySDK.Driver;
+
+namespace alttrashcat_tests_csharp.pages
+{
+    public class GetAnotherChancePage : BasePage
+    {
+        public GetAnotherChancePage(AltDriver driver) : base(driver)
+        {
+        }
+
+        public AltObject GameOverButton { get => Driver.WaitForObject(By.NAME, "GameOver", timeout: 2); }
+        public AltObject PremiumButton { get => Driver.WaitForObject(By.NAME, "Premium Button", timeout: 2); }
+        public AltObject AvailableCurrency { get => Driver.WaitForObject(By.NAME, "PremiumOwnCount", timeout: 2); }
+
+        public bool IsDisplayed()
+        {
+            if (GameOverButton != null && PremiumButton != null && AvailableCurrency != null)
+                return true;
+            return false;
+        }
+        public void PressGameOver()
+        {
+            GameOverButton.Tap();
+        }
+        public void PressPremiumButton()
+        {
+            PremiumButton.Tap();
+        }
+    }
+}
